2dc26c3dbd1d341c21ca196b28871ce67ffbbd7b
Luo Tian <lt@basecity.com>
2023-02-28 13:25:49 +0800

    refactor implmentation details into impl ns

--- src/com/mjdowney/rich_comment_tests.clj
+++ src/com/mjdowney/rich_comment_tests.clj
@@ -5,6 +5,7 @@
             [clojure.string :as string]
             [clojure.test :as test]
             [com.mjdowney.rich-comment-tests.emit-tests :as tests]
+            [com.mjdowney.rich-comment-tests.impl :as impl]
             [rewrite-clj.zip :as z])
   (:import (clojure.lang Namespace)))
 
@@ -72,175 +73,20 @@
 ; The parsing code reads the rich comment test and generates 'test forms':
 ; (clojure.test/is (clojure.core/= (+ 1 1) 2) "Some text")
 
-(defn rct?
-  "Is the node at the `zloc` a ^:rct/test (comment ...) block?"
-  [zloc]
-  (when-let [sexpr (when (z/sexpr-able? zloc) (z/sexpr zloc))]
-    (and (= (first sexpr) 'comment) (:rct/test (meta sexpr)))))
-
-(defn iterate1
-  "Like `clojure.core/iterate`, but stops at the first `nil` element."
-  [f x]
-  (lazy-seq
-    (cons x
-          (when-some [nxt (f x)]
-            (iterate1 f nxt)))))
-
 (defn rct-zlocs
   "Given the root zloc for a source file, return a series of rct zlocs."
   [source-file-root-zloc]
   (->> source-file-root-zloc
-       (iterate1 z/right)
-       (filter rct?)))
-
-(defn test-sexpr-zlocs
-  "All sexpr-able nodes inside a rct form."
-  [rct-zloc]
-  (->>
-    (iterate1
-      z/right
-      (-> rct-zloc
-          z/down
-          z/right
-          z/down
-          z/right))
-    (filter z/sexpr-able?)))
-
-(defn result-comment?
-  "A string like \";=> _\" or \";=>> _\" or \";; => _\""
-  [s]
-  (re-matches #"\s*;+\s?\S*?=>{1,2}.*\n" s))
-
-(defn pairs
-  "Transducer from [a b c ... z] => [[a b] [b c] ... [z nil]]."
-  []
-  (fn [rf]
-    (let [left (volatile! nil)]
-      (fn
-        ([] rf)
-        ([xs x]
-         (let [lft @left]
-           (vreset! left x)
-           (if lft
-             (rf xs [lft x])
-             xs)))
-        ([xs]
-         (rf
-           (if-let [lft @left]
-             (unreduced (rf xs [lft nil]))
-             xs)))))))
-
-(defn rsequence
-  "Like `(reverse (sequence xform coll))`."
-  [xform coll]
-  (transduce xform (completing #(cons %2 %1)) (list) coll))
-
-(defn context-strings
-  "A series of string comments preceding the test sexpr."
-  [test-sexpr-zloc]
-  (let [nodes-preceding-assertion (rest (iterate z/left* test-sexpr-zloc))]
-    (letfn [(form-expstr-pair? [[a b]]
-              (and a b ; is a pair
-                   (z/sexpr-able? b) ; starting with a form
-                   (= (z/tag a) :comment) ; then an expectation string comment
-                   (result-comment? (z/string a))))]
-      (rsequence
-        (comp
-          ; take up to the next completely blank line, ignoring nodes that are
-          ; only whitespace
-          (take-while (complement z/linebreak?))
-          (remove z/whitespace?)
-
-          ; drop any form + expectation string comment pairs
-          (pairs)
-          (remove form-expstr-pair?)
-          (map first)
-
-          ; take all other comments and remove empties
-          (filter z/whitespace-or-comment?)
-          (map z/string)
-          (remove (comp empty? string/trim)))
-        nodes-preceding-assertion))))
-
-(defn advance [zloc & {:keys [to through] :as ops}]
-  {:pre [to through]}
-  (let [zloc (z/right* zloc)]
-    (cond
-      (to zloc) zloc
-      (through zloc) (recur zloc ops)
-      :else nil)))
-
-(defn tag? [x] #(= (z/tag %) x))
-
-(defn ?result-comment-zloc [z]
-  ; Find the next comment after the form, breaking if a node is not whitespace
-  (when-let [z (advance z :to (tag? :comment) :through z/whitespace?)]
-    ; Return the zloc if the comment is a result comment
-    (when (result-comment? (z/string z))
-      z)))
-
-(defn ?comment-expectation-string [z]
-  (let [nodes-following-assertion (iterate z/right* z)
-        ?sequence (comp seq sequence)]
-    (when-let [[fst-line & rest]
-               (?sequence
-                 (comp
-                   (take-while z/whitespace-or-comment?)
-                   (map z/string)
-                   (drop-while (complement result-comment?))
-                   ; stop searching at the first double line break
-                   (take-while (complement #{"\n"}))
-                   ; strip leading ;s from comments
-                   (map #(string/replace-first % #"^\s*;+\s?" "")))
-                 nodes-following-assertion)]
-      (let [[_ _ fst-line] (re-matches #"(?s)(\S*?=>{1,2})(.+)" fst-line)
-            s (string/trim (apply str fst-line rest))]
-        (when (seq s)
-          s)))))
-
-(defn result-comment-type [z]
-  (let [[_ t _] (re-matches #"(?s);+\s*(\S*=>{1,2})(.+)" (z/string z))]
-    (symbol t)))
-
-(defn expectation-data
-  "Parse a string representing the expectation for a test expression and an
-  expression type, returning a vector of `[type str]` (or nil if none).
-
-  The expected result is designated by a =>-prefixed comment, either directly
-  after or on a line following the test expression.
-
-  For example:
-
-    (+ 1 1) ;=> 2
-
-    (+ 1 1)
-    ;;=> 2
-
-    (+ 1 1)
-    ;; => 2"
-  [test-sexpr-zloc]
-  ; Get the zloc for a result comment node ("; => ...") directly following
-  ; the test sexpr, if one exists
-  (when-let [rcz (?result-comment-zloc test-sexpr-zloc)]
-    ; Get the string following the => part of the comment (including to
-    ; following lines) if one exists that isn't empty
-    (if-let [ces (?comment-expectation-string rcz)]
-      [(result-comment-type rcz) ces]
-
-      ; Otherwise, check if there's a sexpr directly following the empty
-      ; result comment
-      (when-let [sexpr (advance rcz
-                         :to z/sexpr-able?
-                         :through (tag? :whitespace))]
-        [(result-comment-type rcz) (z/string sexpr)]))))
+       (impl/iterate1 z/right)
+       (filter impl/rct?)))
 
 (defn rct-data-seq
   "Take an rct zloc and return a series of maps with information about
   tests to run."
   [rct-zloc]
-  (for [zloc (test-sexpr-zlocs rct-zloc)
-        :let [[et es] (expectation-data zloc)]]
-    {:context-strings (context-strings zloc)
+  (for [zloc (impl/test-sexpr-zlocs rct-zloc)
+        :let [[et es] (impl/expectation-data zloc)]]
+    {:context-strings (impl/context-strings zloc)
      :test-sexpr (z/sexpr zloc)
      :expectation-string es
      :expectation-type et
---
+++ src/com/mjdowney/rich_comment_tests/impl.clj
@@ -0,0 +1,158 @@
+(ns com.mjdowney.rich-comment-tests.impl 
+  (:require [clojure.string :as string]
+            [rewrite-clj.zip :as z]))
+
+(defn rct?
+  "Is the node at the `zloc` a ^:rct/test (comment ...) block?"
+  [zloc]
+  (when-let [sexpr (when (z/sexpr-able? zloc) (z/sexpr zloc))]
+    (and (= (first sexpr) 'comment) (:rct/test (meta sexpr)))))
+
+(defn iterate1
+  "Like `clojure.core/iterate`, but stops at the first `nil` element."
+  [f x]
+  (lazy-seq
+   (cons x
+         (when-some [nxt (f x)]
+           (iterate1 f nxt)))))
+
+(defn test-sexpr-zlocs
+  "All sexpr-able nodes inside a rct form."
+  [rct-zloc]
+  (->>
+   (iterate1
+    z/right
+    (-> rct-zloc
+        z/down
+        z/right
+        z/down
+        z/right))
+   (filter z/sexpr-able?)))
+
+(defn result-comment?
+  "A string like \";=> _\" or \";=>> _\" or \";; => _\""
+  [s]
+  (re-matches #"\s*;+\s?\S*?=>{1,2}.*\n" s))
+
+(defn pairs
+  "Transducer from [a b c ... z] => [[a b] [b c] ... [z nil]]."
+  []
+  (fn [rf]
+    (let [left (volatile! nil)]
+      (fn
+        ([] rf)
+        ([xs x]
+         (let [lft @left]
+           (vreset! left x)
+           (if lft
+             (rf xs [lft x])
+             xs)))
+        ([xs]
+         (rf
+          (if-let [lft @left]
+            (unreduced (rf xs [lft nil]))
+            xs)))))))
+
+(defn rsequence
+  "Like `(reverse (sequence xform coll))`."
+  [xform coll]
+  (transduce xform (completing #(cons %2 %1)) (list) coll))
+
+(defn context-strings
+  "A series of string comments preceding the test sexpr."
+  [test-sexpr-zloc]
+  (let [nodes-preceding-assertion (rest (iterate z/left* test-sexpr-zloc))]
+    (letfn [(form-expstr-pair? [[a b]]
+              (and a b ; is a pair
+                   (z/sexpr-able? b) ; starting with a form
+                   (= (z/tag a) :comment) ; then an expectation string comment
+                   (result-comment? (z/string a))))]
+      (rsequence
+       (comp
+          ; take up to the next completely blank line, ignoring nodes that are
+          ; only whitespace
+        (take-while (complement z/linebreak?))
+        (remove z/whitespace?)
+
+          ; drop any form + expectation string comment pairs
+        (pairs)
+        (remove form-expstr-pair?)
+        (map first)
+
+          ; take all other comments and remove empties
+        (filter z/whitespace-or-comment?)
+        (map z/string)
+        (remove (comp empty? string/trim)))
+       nodes-preceding-assertion))))
+
+(defn advance [zloc & {:keys [to through] :as ops}]
+  {:pre [to through]}
+  (let [zloc (z/right* zloc)]
+    (cond
+      (to zloc) zloc
+      (through zloc) (recur zloc ops)
+      :else nil)))
+
+(defn tag? [x] #(= (z/tag %) x))
+
+(defn ?result-comment-zloc [z]
+  ; Find the next comment after the form, breaking if a node is not whitespace
+  (when-let [z (advance z :to (tag? :comment) :through z/whitespace?)]
+    ; Return the zloc if the comment is a result comment
+    (when (result-comment? (z/string z))
+      z)))
+
+(defn ?comment-expectation-string [z]
+  (let [nodes-following-assertion (iterate z/right* z)
+        ?sequence (comp seq sequence)]
+    (when-let [[fst-line & rest]
+               (?sequence
+                (comp
+                 (take-while z/whitespace-or-comment?)
+                 (map z/string)
+                 (drop-while (complement result-comment?))
+                   ; stop searching at the first double line break
+                 (take-while (complement #{"\n"}))
+                   ; strip leading ;s from comments
+                 (map #(string/replace-first % #"^\s*;+\s?" "")))
+                nodes-following-assertion)]
+      (let [[_ _ fst-line] (re-matches #"(?s)(\S*?=>{1,2})(.+)" fst-line)
+            s (string/trim (apply str fst-line rest))]
+        (when (seq s)
+          s)))))
+
+(defn result-comment-type [z]
+  (let [[_ t _] (re-matches #"(?s);+\s*(\S*=>{1,2})(.+)" (z/string z))]
+    (symbol t)))
+
+(defn expectation-data
+  "Parse a string representing the expectation for a test expression and an
+  expression type, returning a vector of `[type str]` (or nil if none).
+
+  The expected result is designated by a =>-prefixed comment, either directly
+  after or on a line following the test expression.
+
+  For example:
+
+    (+ 1 1) ;=> 2
+
+    (+ 1 1)
+    ;;=> 2
+
+    (+ 1 1)
+    ;; => 2"
+  [test-sexpr-zloc]
+  ; Get the zloc for a result comment node ("; => ...") directly following
+  ; the test sexpr, if one exists
+  (when-let [rcz (?result-comment-zloc test-sexpr-zloc)]
+    ; Get the string following the => part of the comment (including to
+    ; following lines) if one exists that isn't empty
+    (if-let [ces (?comment-expectation-string rcz)]
+      [(result-comment-type rcz) ces]
+
+      ; Otherwise, check if there's a sexpr directly following the empty
+      ; result comment
+      (when-let [sexpr (advance rcz
+                                :to z/sexpr-able?
+                                :through (tag? :whitespace))]
+        [(result-comment-type rcz) (z/string sexpr)]))))
